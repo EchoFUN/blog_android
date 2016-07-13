c64e3ae702696c147cc9c2897a4a50d57dabf8bb
xukai <xukai@meituan.com>
2016-07-13 19:57:42 +0800

    Add.

---
+++ app/src/main/java/net/botobe/android/StartActivity.java
@@ -0,0 +1,16 @@
+package net.botobe.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
