4e042f13457995e670f3dbcd9a8c3abcd8379659
EchoFUN <xukai.ken@gmail.com>
2016-09-25 01:53:02 +0800

    折腾的安卓的一些东东

--- app/src/main/java/net/botobe/android/MainActivity.java
+++ app/src/main/java/net/botobe/android/MainActivity.java
@@ -1,5 +1,6 @@
 package net.botobe.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import net.botobe.android.elf.ELF;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,6 +34,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        // 启动服务
+        Intent ELFIntent = new Intent(MainActivity.this, ELF.class);
+        this.startService(ELFIntent);
     }
 
     @Override
